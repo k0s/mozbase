e903501301538a7751cda29083a63176e1e267c2
Jeff Hammel <jhammel@mozilla.com>
2013-04-02 17:06:29 -0700

    finish that one up

--- manifestdestiny/tests/test_read_ini.py
+++ manifestdestiny/tests/test_read_ini.py
@@ -49,14 +49,20 @@ kittens = true # This test requires kittens
         buffer.write(string)
         buffer.seek(0)
         result = read_ini(buffer)[0][1]['kittens']
+        self.assertEqual(result, "true ; This test requires kittens")
 
         # compare this to ConfigParser
-        # python 2.7 ConfigParser
+        # python 2.7 ConfigParser *does* support ';' as an
+        # inline comment delimeter (ibid).
+        # Python 3.x configparser, OTOH, does not support
+        # inline-comments by default.  It does support their specification,
+        # though they are weakly discouraged:
+        # http://docs.python.org/dev/library/configparser.html
         buffer.seek(0)
         parser = ConfigParser()
         parser.readfp(buffer)
         control = parser.get('test_felinicity.py', 'kittens')
-#        self.assertEqual(result, control)
+        self.assertNotEqual(result, control)
 
 
 if __name__ == '__main__':
