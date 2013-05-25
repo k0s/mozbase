953a54eb9f17c48e5c1bf2f24ed72303d9757e5b
Jeff Hammel <jhammel@mozilla.com>
2013-05-24 18:13:46 -0700

    wip

--- mozprofile/tests/test_addon_backup.py
+++ mozprofile/tests/test_addon_backup.py
@@ -11,19 +11,74 @@ and https://bugzilla.mozilla.org/show_bug.cgi?id=841086
 """
 
 import os
+import tempfile
 import unittest
+import zipfile
 
 # directory of this file
 here = os.path.dirname(os.path.abspath(__file__))
 
+def clone_addon(name, output=None, compress=True):
+    """
+    Make a testing addon from the empty addon template
+
+    - name: name of the addon (used in the `id` and `name` fields)
+    - output: path of output file or directory
+    - compress: whether to compress into an XPI or not
+    """
+
+    # source install.rdf file
+    template = os.path.join(here, 'addons', 'empty', 'install.rdf')
+    assert os.path.exists(template), "'%s' not found" % template
+    template = file(template).read()
+
+    # lines to replace in the install.rdf file
+    replace = {'<em:id>test-empty@quality.mozilla.org</em:id>': '<em:id>%s@quality.mozilla.org</em:id>' % name,
+               '<em:name>Test Extension (empty)</em:name>': '<em:name>%s</em:name>' % name
+               }
+    for item, replacement in replace.items():
+        template = template.replace(item, replacement)
+
+    def write_xpi(contents, path):
+        """write the xpi file to `path`"""
+        import pdb; pdb.set_trace()
+
+    # write it!
+    # note: this pattern could be abstracted
+    if output:
+        if os.path.exists(output):
+            if os.path.isdir(output):
+                if compressed:
+                    path = os.path.join(output, 'name.xpi')
+                else:
+                    with file(os.path.join(output, 'install.rdf'), 'w') as f:
+                        f.write(template)
+                    return output
+            else:
+                if not compressed:
+                    raise AssertionError("Specified directory format for addon but you gave a file path: '%s'" % output)
+                path = output
+            write_xpi(template, path)
+            return path
+        else:
+            if compressed:
+                dirname = os.path.dirname(output)
+                if dirname and not os.path.isdir(dirname):
+                    os.makedirs(dirname)
+                write_xpi(output)
+                return output
+            else:
+                os.makedirs(output)
+                with file(os.path.join(output, 'install.rdf'), 'w') as f:
+                    f.write(template)
+                return output
+    else:
+        if compressed:
+            pass
+        else:
+            tmppath = 
 
 class TestAddonBackup(unittest.TestCase):
-
-    def clone_addon(self, name, compress=True):
-        """
-        - name: name of the addon (e.g. 'empty' for the two in there)
-        - compress: whether to compress into an XPI or not
-        """
 
     def test_addon_backup(self):
         """
