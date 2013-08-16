92cf7ed79c605e5df65ffbe443aa456adc413c47
Jeff Hammel <jhammel@mozilla.com>
2013-08-16 14:56:11 -0700

    wip

--- manifestdestiny/tests/test_manifestparser.py
+++ manifestdestiny/tests/test_manifestparser.py
@@ -112,9 +112,12 @@ class TestManifestparser(unittest.TestCase):
         include = os.path.join(here, 'include-relative')
         manifest = os.path.join(include, 'manifest.ini')
         parser = ManifestParser(manifests=(manifest,))
-        self.assertEqual(len(parser.tests), 1)
+        self.assertEqual(len(parser.tests), 2)
         test = os.path.join(include, 'bar', 'bar.js')
         self.assertEqual(test, parser.tests[0]['path'])
+        test = os.path.join(include, 'foo', 'test.js')
+        self.assertEqual(test, parser.tests[1]['path'])
+
 
     def test_directory_to_manifest(self):
         """
