fcdaecc257fd44e31b1f43d74d6d3dbccc804fa1
Jeff Hammel <jhammel@mozilla.com>
2013-11-19 17:10:11 -0800

    wip

--- mozprocess/tests/test_plumbing.py
+++ mozprocess/tests/test_plumbing.py
@@ -5,6 +5,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import sys
 import unittest
 
 here = os.path.dirname(os.path.abspath(__file__))
@@ -13,6 +14,9 @@ class TestPlumbing(unittest.TestCase):
     """
     test piping mozprocess subprocesses
     """
+
+    def command(self, command, *args):
+        return [sys.executable, command] + list(args)
 
     def test_pipe(self):
         pass
