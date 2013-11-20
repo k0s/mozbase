30492d93ef3cf005e10e0d7d9d04d9bf11ff377b
Jeff Hammel <jhammel@mozilla.com>
2013-11-20 11:48:19 -0800

    wip

--- mozprocess/tests/test_plumbing.py
+++ mozprocess/tests/test_plumbing.py
@@ -4,11 +4,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import mozprocess
 import os
 import sys
 import unittest
 
 here = os.path.dirname(os.path.abspath(__file__))
+
+class OutputHandler(object):
+    """
+    handler to store lines of output
+    """
+    def __init__(self):
+        self.lines = []
+    def __call__(self, line):
+        self.lines.append(line)
 
 class TestPlumbing(unittest.TestCase):
     """
@@ -16,10 +26,19 @@ class TestPlumbing(unittest.TestCase):
     """
 
     def command(self, command, *args):
-        return [sys.executable, command] + list(args)
+        return [sys.executable, os.path.join(here, command)] + list(args)
 
     def test_pipe(self):
-        pass
+        """
+        test piping one subprocess to another; see
+        https://bugzilla.mozilla.org/show_bug.cgi?id=924253
+        """
+
+        process = mozprocess.ProcessHandlerMixin(self.command('count.py', '11'))
+        process.run()
+        pipe = mozprocess.ProcessHandler(self.command('toupper.py'), stdin=process.proc.stdout)
+        pipe.run()
+        status = process.wait()
 
 if __name__ == '__main__':
     unittest.main()
